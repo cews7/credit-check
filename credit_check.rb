7256edff99d6a80b9ecfffa9c311bf27df842069
Eric Wahlgren-Sauro <cwahlgrensauro@gmail.com>
2016-10-06 14:55:06 -0600

    complete credit_check

--- credit_check.rb
+++ credit_check.rb
@@ -4,16 +4,8 @@ new_card_number = split_card_number.map.with_index do |num, index|
   index.even? ? num * 2 : num
 end
 summed_digits_over_ten = new_card_number.map do |num|
-  if num > 9
-    num = num.to_s.split("")
-    num = num[0].to_i + num[1].to_i
-  else
-    num
-  end
+  num > 9 ? num -= 9 : num
 end
-  total = summed_digits_over_ten.reduce(:+) % 10
-  if total == 0
-    puts "card is valid"
-  else
-    puts "card is invalid"
-  end
+total = summed_digits_over_ten.reduce(:+) % 10
+puts "card is valid" if total == 0
+puts "card is invalid" if total != 0
